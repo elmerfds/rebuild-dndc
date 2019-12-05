83d254f537831dde0068b772dc8f87724e8e78be
elmerfdz <elmerfdz@users.noreply.github.com>
2019-12-05 02:24:24 +0000

    Improvements to Rebuild-manual script
    Now supports force rebuild (i.e. stop/remove/rebuild container) see Readme.

--- Rebuild-DNDC/Rebuild-DNDC-Manual.sh
+++ Rebuild-DNDC/Rebuild-DNDC-Manual.sh
@@ -1,22 +1,90 @@
 #!/bin/bash
 #author: https://github.com/elmerfdz
-ver=1.1.0
+ver=2.0.0
 
 #VARS
-inscope_containers=("$@") 
+inscope_containers=("$@")
+force=0
+dryrun=0
 
+rebuild_man()
+{
 echo
 echo "-------------------------------"
 echo " Rebuild-DNDC-Manual v$ver     "
 echo "-------------------------------"
 echo
 
-for ((a=0; a < "${#inscope_containers[@]}"; a++)) 
-do
-    inscope_cont_tmpl+=($(find $docker_tmpl_loc -type f -iname "*-${inscope_containers[$a]}.xml"))
-    echo
-    echo "--Build ${inscope_containers[$a]}"
-    echo
-    $rundockertemplate_script -v ${inscope_cont_tmpl[$a]}
-    echo
+if [ "$force" == "0" ]
+then
+    inscope_containers_purify=()
+    for value in "${inscope_containers[@]}"
+    do
+        [[ $value != "-b" ]] && inscope_containers_purify+=($value)
+    done
+    inscope_containers=("${inscope_containers_purify[@]}")
+    unset inscope_containers_purify
+    
+    for ((a=0; a < "${#inscope_containers[@]}"; a++))
+    do
+        inscope_cont_tmpl+=($(find $docker_tmpl_loc -type f -iname "*-${inscope_containers[$a]}.xml"))
+        echo
+        echo "--Build ${inscope_containers[$a]}"
+        echo
+        $rundockertemplate_script -v ${inscope_cont_tmpl[$a]}
+        echo
+    done
+elif [ "$force" == "1" ]
+then
+        inscope_containers_purify=()
+        for value in "${inscope_containers[@]}"
+        do
+            [[ $value != "-f" ]] && inscope_containers_purify+=($value)
+        done
+        inscope_containers=("${inscope_containers_purify[@]}")
+        unset inscope_containers_purify
+    for ((a=0; a < "${#inscope_containers[@]}"; a++))
+    do
+        inscope_cont_tmpl+=($(find $docker_tmpl_loc -type f -iname "*-${inscope_containers[$a]}.xml"))
+        build_stage_var=('Stopping' 'Removing' 'Recreating')
+        echo
+        echo "--Build ${inscope_containers[$a]}"
+        echo
+         for ((d=0; d < "${#build_stage_var[@]}"; d++))
+            do
+                buildcont_cmd="$rundockertemplate_script -v ${inscope_cont_tmpl[$a]}"
+                build_stage_cmd_var=("docker stop ${inscope_containers[$a]}" "docker rm ${inscope_containers[$a]}" "$buildcont_cmd")
+                build_stage=${build_stage_var[$d]}
+                build_stage_cmd=${build_stage_cmd_var[$d]}
+            echo
+            echo "----------------------------"
+            echo "  $build_stage $contname   "
+            echo "----------------------------"
+            echo
+            $build_stage_cmd
+            #echo "build_stage_cmd: $build_stage_cmd"
+        echo
+            done
+    done
+fi
+}
+
+while getopts f:b: opt; do
+    case $opt in
+	h)
+	    usage
+	    exit
+	    ;;
+	f)
+	    force=1
+        rebuild_man
+	    ;;
+	b)
+	    force=0
+        rebuild_man
+	    ;;
+    esac
 done
+
+
+
