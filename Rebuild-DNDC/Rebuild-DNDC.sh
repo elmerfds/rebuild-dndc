8fe544e6a634ac4a83dde8cb4432704066ce4876
elmerfdz <elmerfdz@users.noreply.github.com>
2020-03-14 02:50:35 +0000

    Update Rebuild-DNDC.sh

--- Rebuild-DNDC/Rebuild-DNDC.sh
+++ Rebuild-DNDC/Rebuild-DNDC.sh
@@ -137,7 +137,7 @@ inscope_container_vars()
                     list_inscope_cont_ids+=(${get_container_ids[$a]})
                     list_inscope_contnames+=(${get_container_names[$a]})     
                     no=${#list_inscope_contnames[@]}
-                    echo "$no.${get_container_names[$a]}"
+                    echo "$no. ${get_container_names[$a]}"
                     echo "  - ContainerID ${get_container_ids[$a]}"       
                     echo "  - NetworkID: $pull_contnet_ids"              
                     echo "  - Template Location: ${list_inscope_cont_tmpl[$b]}"; b=$((b + 1))       
