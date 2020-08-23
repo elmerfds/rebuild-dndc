b50c6197687e76a83579ac8e3ce158f66f4a0721
elmerfdz <elmerfdz@users.noreply.github.com>
2020-08-23 19:28:21 +0100

    Update Rebuild-DNDC.sh

--- Rebuild-DNDC/Rebuild-DNDC.sh
+++ Rebuild-DNDC/Rebuild-DNDC.sh
@@ -309,8 +309,8 @@ app_pf()
             fi    
         done     
         rutorrent_rc_loc=$(find $pf_loc/rutorrent/ -type f -iname "*rtorrent.rc")
-        rutorrent_pf_status=$(grep -q "port_range = $vpn_pf-$vpn_pf" "$rutorrent_rc_loc" ; echo $?)
-        rutorrent_ip_status=$(grep -q "ip = $vpn_wanip" "$rutorrent_rc_loc" ; echo $?)        
+        rutorrent_pf_status=$(grep -w "port_range = $vpn_pf-$vpn_pf" "$rutorrent_rc_loc" ; echo $?)
+        rutorrent_ip_status=$(grep -w "ip = $vpn_wanip" "$rutorrent_rc_loc" ; echo $?)        
         if [ "$rutorrent_pf_status" == "1" ] || [ "$rutorrent_ip_status" == "1" ]
         then
             if [ "$rutorrent_pf_status" == "1" ]
